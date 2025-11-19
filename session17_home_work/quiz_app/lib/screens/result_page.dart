d28c596e3db528f71b949225623763e5e0175aac
raniafahmy20 <120285519+raniafahmy20@users.noreply.github.com>
2025-11-19 15:21:29 +0200

    done

--- session17_home_work/quiz_app/lib/screens/result_page.dart
+++ session17_home_work/quiz_app/lib/screens/result_page.dart
@@ -3,7 +3,6 @@ import 'package:quiz_app/models/quiz_app.dart';
 import 'package:quiz_app/screens/home_page.dart';
 import 'package:quiz_app/screens/question_page.dart';
 import 'package:quiz_app/thems/asset.dart';
-import 'package:quiz_app/thems/color.dart';
 import 'package:quiz_app/thems/decoration.dart';
 import 'package:quiz_app/thems/text_style.dart';
 import 'package:quiz_app/widgets/custom_button.dart';
@@ -14,44 +13,60 @@ class ResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: DecorationModel.mainDecoratuoin(),
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(AssetModel.questionPageHome),
+    double correctAnswers = quizApp.calculateResult();
+    double numberOfQuestions = quizApp.questions.length.toDouble();
+    double percent =
+        (quizApp.calculateResult() / quizApp.questions.length) * 100;
+    return Scaffold(
+      body: Container(
+        decoration: DecorationModel.mainDecoratuoin(),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(AssetModel.questionPageHome),
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 20,
-          children: [
-            CircleAvatar(
-              radius: 70,
-              backgroundColor: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Column(
-                  children: [
-                    Text(
-                      '${quizApp.calculateResult()}',
-                      style: TextStyleModel.medium24H1(ColorModel.mainViolet),
-                    ),
-                    Text(
-                      '/',
-                      style: TextStyleModel.medium24H1(ColorModel.mainViolet),
-                    ),
-                    Text(
-                      '${quizApp.questions.length}',
-                      style: TextStyleModel.medium24H1(ColorModel.mainViolet),
-                    ),
-                  ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Quiz Results",
+                style: TextStyleModel.medium24H1(Colors.white),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 140,
+                width: 140,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Text(
+                    "$correctAnswers\n/$numberOfQuestions",
+                    textAlign: TextAlign.center,
+                    style: TextStyleModel.medium24H1(Colors.white),
+                  ),
                 ),
               ),
-            ),
-            CustomButton(buttonName: 'Reset Quiz', page: QuestionPage()),
-            CustomButton(buttonName: 'Home Page', page: HomePage()),
-          ],
+              const SizedBox(height: 20),
+              Text(
+                "${percent.toStringAsFixed(0)}%",
+                style: TextStyleModel.medium24H1(Colors.white),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "Keep Practicing!",
+                style: TextStyleModel.medium18H2(Colors.white),
+              ),
+              const SizedBox(height: 30),
+              const Icon(Icons.check, color: Colors.white, size: 70),
+              const SizedBox(height: 50),
+              CustomButton(buttonName: 'Back to Home', page: HomePage()),
+              const SizedBox(height: 15),
+              CustomButton(buttonName: 'Restart Quiz', page: QuestionPage()),
+            ],
+          ),
         ),
       ),
     );
